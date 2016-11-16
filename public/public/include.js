cf4ecbe53839e0b68640afe50fbb69967c1d2dcc
Claes Östlund <claes@stenbergit.se>
2016-11-16 21:51:45 +0100

    Change load method to jQuery because of Mac issues.

---
+++ public/public/include.js
@@ -0,0 +1,64 @@
+function plugin_wf_ajax(){
+  this.elements = new Array();
+  /**
+   * Load to element.
+   * @param {type} id
+   * @param {type} url
+   * @returns {undefined}
+   */
+  this.load = function (id, url){
+    /**
+     * Set element in array.
+     */
+    this.elements[id] = url;
+    /**
+     * Handle url.
+     */
+    var time = new Date().getTime();
+    if(url.indexOf('?')>0){url=url+'&_time='+time;}else{url=url+'?_time='+time;}
+    /**
+     * Loading gif.
+     */
+    if(document.getElementById(id)){
+      document.getElementById(id).innerHTML='<img style="margin:10px;" src="/plugin/wf/ajax/loading.gif">';
+    }
+    /**
+     * Ajax request.
+     */
+    $.get(url, function(data){
+      var element = document.getElementById(id);
+      if(!element){
+        alert('Could not find element with id '+id+'!');
+      }else{
+        element.innerHTML = data;
+        var scripts = element.getElementsByTagName('script');
+        for (var i=0;i<scripts.length;i++) {
+          eval(scripts[i].innerHTML);
+        }
+      }
+    });    
+  }
+  /**
+   * Update an element.
+   * @param {type} id
+   * @returns {undefined}
+   */
+  this.update = function(id){
+    if(this.elements[id]){
+      this.load(id, this.elements[id]);
+    }
+  }
+  /**
+   * Request to method PluginWfCallbackjson.call().
+   * @param {type} url
+   * @returns {Boolean}
+   */
+  this.callbackjson = function(url){
+    $.get(url).done(function(data) 
+    { 
+      PluginWfCallbackjson.call( data ); 
+    });
+    return false;
+  }
+}
+PluginWfAjax = new plugin_wf_ajax();

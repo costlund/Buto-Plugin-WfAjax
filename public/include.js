47feafe15125f3a7859cb8c39a765090f2d00594
costlund <claes@webforms.se>
2016-12-01 11:34:35 +0100

    Bug fix.

--- public/include.js
+++ public/include.js
@@ -7,39 +7,36 @@ function plugin_wf_ajax(){
    * @returns {undefined}
    */
   this.load = function (id, url){
-    this.myListener = function(){
+    /**
+     * Set element in array.
+     */
+    this.elements[id] = url;
+    /**
+     * Handle url.
+     */
+    var time = new Date().getTime();
+    if(url.indexOf('?')>0){url=url+'&_time='+time;}else{url=url+'?_time='+time;}
+    /**
+     * Loading gif.
+     */
+    if(document.getElementById(id)){
+      document.getElementById(id).innerHTML='<img style="margin:10px;" src="/plugin/wf/ajax/loading.gif">';
+    }
+    /**
+     * Ajax request.
+     */
+    $.get(url, function(data){
       var element = document.getElementById(id);
       if(!element){
-        console.log('PluginWfAjax could not find element with id '+id+' to load responseText in!');
+        console.log('PluginWfAjax says: Could not find element with id '+id+'!');
       }else{
-        element.innerHTML = this.responseText;
+        element.innerHTML = data;
         var scripts = element.getElementsByTagName('script');
         for (var i=0;i<scripts.length;i++) {
           eval(scripts[i].innerHTML);
         }
       }
-    }
-    var xmlhttp = null;
-    if (window.XMLHttpRequest)
-    {
-      xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    this.elements[id] = url;
-    // Set _time param.
-    var time = new Date().getTime();
-    if(url.indexOf('?')>0){url=url+'&_time='+time;}else{url=url+'?_time='+time;}
-    if(document.getElementById(id)){
-      document.getElementById(id).innerHTML='<img style="margin:10px;" src="/plugin/wf/ajax/loading.gif">';
-    }
-    xmlhttp.open("GET",url,false);
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("Content-Length", 999);   
-    xmlhttp.onload = this.myListener;
-    xmlhttp.send();
+    });    
   }
   /**
    * Update an element.
